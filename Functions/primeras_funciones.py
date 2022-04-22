85e16a3ae5b27ba497e3200e2d1782afa89e61ef
Abraham Márquez <94422016+emiliano550@users.noreply.github.com>
2022-04-22 16:17:06 -0500

    Add files via upload

---
+++ Functions/primeras_funciones.py
@@ -0,0 +1,64 @@
+#def mensaje():
+#    print('Estamos aprendiendo Python')
+#    print('Estamos aprendiendo instrucciones basicas')
+#    print('Poco a poco iremos avanzando')
+
+#mensaje()
+#mensaje()
+#print('Ejecutando codigo fuera de función')
+#mensaje()
+
+
+#def suma(num1, num2 ):
+#    num1 = 5
+#    num2 = 7
+#    print(num1 + num2)
+#suma()
+
+def resta():
+    num1= 20
+    num2 = 30
+    print(num1 - num2)
+resta()
+
+def mult():
+    num1 = 20
+    num2 = 5
+    print(num1 * num2)
+mult()
+
+def div():
+    num1 = 10 
+    num2= 15
+    print(num1 / num2)
+div()
+
+print(' ')
+
+
+def suma(num1, num2 ):
+    print(num1 + num2)
+
+suma(7, 9)
+# Se puede llamar a la función con parametros independientes
+
+
+def suma1(num1, num2, num3):
+    print(num1 + num2 + num3)
+
+suma1(8, 48, 9)
+print( ' ')
+
+
+def suma2(num1, num2):
+    resultado = num1 + num2
+    return resultado
+
+almacena_resultado = suma2(5, 8)
+print(almacena_resultado)
+
+
+
+
+
+

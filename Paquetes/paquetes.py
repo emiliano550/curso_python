85c5283cf823ebc68a5209b57755b15003d1306c
Abraham Márquez <94422016+emiliano550@users.noreply.github.com>
2022-04-22 16:19:33 -0500

    Add files via upload

---
+++ Paquetes/paquetes.py
@@ -0,0 +1,11 @@
+# PAQUETES 
+
+#¿Qué son?:
+# Directorios o carpetas donde se almacenarán módulos relacionados entre si. 
+
+# ¿Para que sirven?
+# Para organizar y reutilizar tu tu código(módulos)
+
+#¿Cómo se crea un paquete?
+# Tan sencillo como crear una carpeta con un archivo  __init__.py
+

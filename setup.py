52a78c3658ede10d1e89451e715b24984ba81ad0
Abraham Márquez <94422016+emiliano550@users.noreply.github.com>
2022-04-22 16:25:16 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'paquetecalculos',
+    version = '1.0',
+    description = 'Paquete de redondeo y potencia',
+    author = 'Emiliano',
+    author_email= 'emiliano.marquez@gmail.com',
+    url= 'www.youtube.com',
+    packages = ['calculos', 'calculos.redondeo_potencia']
+    )
+
+
+# CREACIÓN DE PAQUETE DISTRIBUIBLE
+#Este archivo setup, es el que va a describir la configuración del paquete distribuible
+
